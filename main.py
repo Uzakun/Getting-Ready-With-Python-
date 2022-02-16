326a1d282cabd473bb77ed63eaa72db2255e1856
Shwetank23 <>
2022-02-16 12:54:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#Write your code below this line 👇
+#print("You are noob\n You are noob\n  You are noob")
+#print("Shwetank" + "Chandra")
+#print("Shwetank" + " " + "Chandra") you can also do same by putting space at last of 'K' and space before the start of 'C'
+#print("Hello " + input("What is your name?\n") + "! Nice to meet you.")
+#input("How are you?\n")
+#We use len() function to count number of charaters in the string , example if we write print(len("Jack")) it will show 4 as output
+#print(len("Hazel"))
+#print( len( input("What is your name? ") ) ) the one that is inside will work first then its outter then the outer most, here first "input" will execute and will work, then the "len" function then at last "print" function will work.
+#Variables:-
+#Name = input("what is your name? ")
+#print(Name)
+#name = "Shwetank"
+#print(name)
+# Name = input("what is your name? ")
+# length = len(Name)
+# print(length) works same as line 9.
